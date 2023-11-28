426506711bce5bd29fb6307e2ff5dbb6040e1b79
7034megatron <emmanuelmabhena0@gmail.com>
2023-11-28 11:42:03 -0800

    corrected lines space

--- 0x01-python-if_else_loops_functions/7-islower.py
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -3,7 +3,7 @@
 def islower(c):
     return ord('a') <= ord(c) <= ord('z')
 
-# Test cases
+
 print("a is {}".format("lower" if islower("a") else "upper"))
 print("H is {}".format("lower" if islower("H") else "upper"))
 print("A is {}".format("lower" if islower("A") else "upper"))
