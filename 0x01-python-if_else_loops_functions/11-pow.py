54e2ed58fa6176663df7cd405b607675f357085f
7034megatron <emmanuelmabhena0@gmail.com>
2023-11-28 13:12:33 -0800

    11

--- 0x01-python-if_else_loops_functions/11-pow.py
+++ 0x01-python-if_else_loops_functions/11-pow.py
@@ -2,6 +2,7 @@
 
 def pow(a, b):
     result = 1
+    tolerance = 1e-15  # Adjust this value based on your precision requirements
 
     if b < 0:
         a = 1 / a
@@ -9,5 +10,8 @@ def pow(a, b):
 
     for _ in range(b):
         result *= a
+
+    if abs(result - round(result)) < tolerance:
+        result = round(result)
 
     return result
