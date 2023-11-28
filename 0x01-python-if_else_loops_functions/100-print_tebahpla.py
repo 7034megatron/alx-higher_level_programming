1ed739aa9e16d546a32cf7df5ff07e989c0fefac
7034megatron <emmanuelmabhena0@gmail.com>
2023-11-28 12:49:32 -0800

    tried to update

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 
-for c in range(ord('z'), ord('A') - 1, -1):
-    print("{:c}".format(c) if c % 2 == 0 else "{:c}".format(c).lower(), end="")
+for c in range(ord('z'), ord('a') - 1, -1):
+    print("{:c}".format(c) + "{:c}".format(c - 32), end="")
