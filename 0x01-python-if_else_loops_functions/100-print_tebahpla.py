9b0c9af30dd582715ad99b95c1ab4573a530772d
7034megatron <emmanuelmabhena0@gmail.com>
2023-11-28 12:51:54 -0800

    3rd attempt

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
 
 for c in range(ord('z'), ord('a') - 1, -1):
-    print("{:c}".format(c) + "{:c}".format(c - 32), end="")
+    print("{:c}".format(c) + "{:c}".format(c - 25), end="")
+
