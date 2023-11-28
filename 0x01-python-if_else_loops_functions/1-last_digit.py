9b25336a8d58774a1a680abf8851ff1a3eee9d14
7034megatron <emmanuelmabhena0@gmail.com>
2023-11-28 09:04:18 -0800

    converted last number to an interger

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -2,12 +2,9 @@
 import random
 
 number = random.randint(-10000, 10000)
-lastDigit = number % 10
+lastDigit = abs(number) % 10
 
-if number < 0:
-    lastDigit = str(lastDigit)[1:]
-
-if lastDigit > 5:
+if int(lastDigit) > 5:
     print("Last digit of", number, "is", lastDigit, "and is greater than 5")
 elif lastDigit == 0:
     print("Last digit of", number, "is", lastDigit, "and is 0")
