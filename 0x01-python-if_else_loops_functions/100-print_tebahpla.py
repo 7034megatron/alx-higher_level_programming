fffe2de9779f508bcd7235df1b9811dfd71f05f3
7034megatron <emmanuelmabhena0@gmail.com>
2023-11-28 12:55:27 -0800

    final

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
 
 for c in range(ord('z'), ord('a') - 1, -1):
-    print("{:c}".format(c) + "{:c}".format(c - 25), end="")
-
+    print("{:c}".format(c) if (ord('z') - c) % 2 == 0 else "{:c}".format(c - 32), end="")
