447f0104ee5755262a6d1e5da7b3ca4b4c077112
7034megatron <emmanuelmabhena0@gmail.com>
2023-11-29 05:51:50 -0800

    add variable

--- 0x00-python-hello_world/7-edges.py
+++ 0x00-python-hello_world/7-edges.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 word = "Holberton"
-print(f"First 3 letters: {word[:3]}")
-print(f"Last 2 letters: {word[-2:]}")
-print(f"Middle word: {word[1:-1]}")
+word_first_3 = print(f"First 3 letters: {word[:3]}")
+word_last_2 = print(f"Last 2 letters: {word[-2:]}")
+middle_word = print(f"Middle word: {word[1:-1]}")
