5b9c47f63397179cd422ef48973251487a26482b
7034megatron <emmanuelmabhena0@gmail.com>
2023-11-30 12:00:20 -0800

    isd

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,16 +1,8 @@
 #!/usr/bin/python3
-import dis
-
 if __name__ == "__main__":
-    with open('hidden_4.pyc', 'rb') as file:
-        code = file.read()
-
-    instructions = dis.get_instructions(code)
-    names = set()
-
-    for instruction in instructions:
-        if instruction.opname == 'LOAD_NAME' and not instruction.argrepr.startswith('__'):
-            names.add(instruction.argrepr)
-
-    for name in sorted(names):
-        print(name)
+    import hidden_4
+    # Print sorted name from directory
+    for name in sorted(dir(hidden_4)):
+        # print only names that do not start with __
+        if name[:2] != '__':
+            print("{}".format(name))
