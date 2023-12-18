c1539ce144e2d2d60207e240263dc0464d898af9
7034megatron <emmanuelmabhena0@gmail.com>
2023-12-18 01:14:04 -0800

    Check 0

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -2,9 +2,11 @@
 
 def safe_print_list(my_list=[], x=0):
     results = []
+    count = 0
     try:
         for i in range(x):
             print(my_list[i], end = "")
+            int(my_list[i])
             result.append(my_list[i])
 
     except Exception:
