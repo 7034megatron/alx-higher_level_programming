acbaa00bd0da5bfff42a90d82e4d12757eba9a68
7034megatron <emmanuelmabhena0@gmail.com>
2023-12-18 01:34:25 -0800

    removed whitespace

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -2,16 +2,13 @@
 
 def safe_print_list_integers(my_list=[], x=0):
     count = 0
-    
     try:
         for i in range(x):
             if type(my_list[i]) == int:
-                print("{:d}".format(my_list[i]), end=" ")
+                print("{:d}".format(my_list[i]), end="")
                 count += 1
     except (IndexError, ValueError):
         pass  # Handle IndexError or ValueError by doing nothing
-    
     finally:
         print()  # Add a newline after printing integers
-    
     return count
