9ba844df89e78a48dfe2eab8933849f216622b26
7034megatron <emmanuelmabhena0@gmail.com>
2023-12-18 11:50:08 -0800

    added zero division error

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -4,7 +4,7 @@ def safe_print_division(a, b):
     result = None
     try:
         result = a / b
-    except:
+    except ZeroDivisionError:
         pass
     finally:
         if result is not None:
