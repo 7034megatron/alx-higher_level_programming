7a1282fbcb506c1ebcd8be9a5422a3d08d8b72c2
7034megatron <emmanuelmabhena0@gmail.com>
2023-12-18 01:29:59 -0800

    work 2:

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -6,6 +6,6 @@ def safe_print_list_integers(my_list=[], x=0):
         for i in range(x):
             print("{:d}".format(my_list[i]))
             result.append(my_list[i])
-    except:
+    except Exception:
         pass
     return len(result)
