e0a4d8ea753ba0c60a0dd5d7dae3b691d6ca7d10
7034megatron <emmanuelmabhena0@gmail.com>
2023-12-18 01:08:16 -0800

    Partially done

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,12 +1,14 @@
 #!/usr/bin/python3
 
 def safe_print_list(my_list=[], x=0):
-
+    results = []
     try:
         for i in range(x):
             print(my_list[i], end = "")
+            result.append(my_list[i])
 
     except Exception:
         pass: 
 
     print() 
+    return results
---
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+def safe_print_list_integers(my_list=[], x=0):
+    result = []
+    try:
+        for i in range(x):
+            print("{:d}".format(my_list[i]))
+            result.append(my_list[i])
+    except:
+        pass
+    return result
