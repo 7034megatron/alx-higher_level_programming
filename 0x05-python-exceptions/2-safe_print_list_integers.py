c208fbcb4623c0526365ab227ed152b9f075489a
7034megatron <emmanuelmabhena0@gmail.com>
2023-12-18 01:38:14 -0800

    styling#2

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -11,4 +11,4 @@ def safe_print_list_integers(my_list=[], x=0):
         pass  # Handle IndexError or ValueError by doing nothing
     finally:
         print()  # Add a newline after printing integers
-    return count
+        return count
