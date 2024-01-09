37bccf97264d65c3473557683835874cce74377d
7034megatron <emmanuelmabhena0@gmail.com>
2024-01-09 04:58:34 -0800

    added documentation

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -1,6 +1,11 @@
 #!/usr/bin/python3
 
+"""
+This program is for writing output
+"""
+
 def write_file(filename="", text=""):
+    """This function writes to the standart output"""
     with open('filename', 'w', encoding="utf-8") as file:
         for line in file:
             file.write(text)
