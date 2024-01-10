fe398ab39709ff29add07bb89fce6b91175ddc7d
7034megatron <emmanuelmabhena0@gmail.com>
2024-01-10 08:46:56 -0800

    updated code

--- 0x07-python-test_driven_development/101-lazy_matrix_mul.py
+++ 0x07-python-test_driven_development/101-lazy_matrix_mul.py
@@ -1,14 +1,22 @@
 #!/usr/bin/python3
-"""This module defines a matrix multiplication function using NumPy."""
+"""Defines a matrix multiplication function using NumPy."""
 import numpy as np
 
 
 def lazy_matrix_mul(m_a, m_b):
-    """Return the the multiplied value of 2 matrices.
+    """Return the multiplication of two matrices using NumPy.
 
     Args:
         m_a (list of lists of ints/floats): The first matrix.
         m_b (list of lists of ints/floats): The second matrix.
-    """
 
-    return (np.matmul(m_a, m_b)
+    Returns:
+        np.ndarray: The result of the matrix multiplication.
+    """
+    try:
+        return np.matmul(m_a, m_b)
+    except ValueError as e:
+        raise ValueError("shapes {} and {} not aligned: {} (dim 1) != {} (dim 0)".format(
+            np.shape(m_a), np.shape(m_b), np.shape(m_a)[1], np.shape(m_b)[0])) from e
+    except Exception as e:
+        raise type(e)("invalid type for matrix multiplication") from e
