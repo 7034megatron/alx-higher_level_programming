8a36727e4db2d4ccf36c81dbfb2ff62fa4b4883a
7034megatron <emmanuelmabhena0@gmail.com>
2024-02-13 09:59:57 -0800

    second table

---
+++ 0x0D-SQL_introduction/4-first_table.sql
@@ -0,0 +1,2 @@
+-- Script that creates table into My SQL server
+CREATE TABLE IF NOT EXISTS `first_table` (`id` INT, `name` VARCHAR(256));
---
+++ 0x0D-SQL_introduction/8-count_89.sql
@@ -0,0 +1,2 @@
+-- Number of records in  first_table where database = 89
+SELECT COUNT (*) FROM `first_table` WHERE `id` = 89;
