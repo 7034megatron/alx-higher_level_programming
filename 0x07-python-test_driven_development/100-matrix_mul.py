604e375d7199136fd1ab4f2d0f81c6c40ef47da8
7034megatron <emmanuelmabhena0@gmail.com>
2024-02-13 05:51:54 -0800

    moved to correct repsitory

--- 0x07-python-test_driven_development/100-matrix_mul.py
+++ 0x07-python-test_driven_development/100-matrix_mul.py
@@ -1,5 +1,34 @@
 #!/usr/bin/python3
-matrix_mul = __import__('100-matrix_mul').matrix_mul
+"""Defines a matrix multiplication function."""
+def matrix_mul(m_a, m_b):
+    """Multiply two matrices.
 
-print(matrix_mul([[1, 2], [3, 4]], [[1, 2], [3, 4]]))
-print(matrix_mul([[1, 2]], [[3, 4], [5, 6]]))
+    Args:
+        m_a (list of lists of ints/floats): The first matrix.
+        m_b (list of lists of ints/floats): The second matrix.
+
+    Returns:
+        list of lists of ints/floats: The result of the matrix multiplication.
+
+    Raises:
+        TypeError: If m_a or m_b is not a list or a list of lists.
+        ValueError: If m_a or m_b is empty or not a rectangle or can't be multiplied.
+    """
+    if not all(isinstance(row, list) for row in m_a) or not all(isinstance(row, list) for row in m_b):
+        raise TypeError("m_a must be a list of lists or m_b must be a list of lists")
+
+    if not m_a or not m_b:
+        raise ValueError("m_a can't be empty or m_b can't be empty")
+
+    if not all(isinstance(num, (int, float)) for row in m_a for num in row) or not all(isinstance(num, (int, float)) for row in m_b for num in row):
+        raise TypeError("m_a should contain only integers or floats or m_b should contain only integers or floats")
+
+    if not all(len(row) == len(m_a[0]) for row in m_a) or not all(len(row) == len(m_b[0]) for row in m_b):
+        raise TypeError("each row of m_a must be of the same size or each row of m_b must be of the same size")
+
+    if len(m_a[0]) != len(m_b):
+        raise ValueError("m_a and m_b can't be multiplied")
+
+    result = [[sum(a * b for a, b in zip(row_a, col_b)) for col_b in zip(*m_b)] for row_a in m_a]
+
+    return result
