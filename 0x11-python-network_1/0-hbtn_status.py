73a47d3604a9b2f00bb2e823f483813282a712f0
7034megatron <emmanuelmabhena0@gmail.com>
2024-03-28 09:47:44 -0700

    fetch or GET data

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Fetch data from the given website and format it as specified"""
+from urllib.request import urlopen
+
+with urlopen("https://alx-intranet.hbtn.io/status") as response:
+    body = response.read()
+    print("Body response:")
+    print("\t- type: {}".format(type(body)))
+    print("\t- content: {}".format(body))
+    print("\t- utf8 content: {}".format(body.decode("utf-8")))
