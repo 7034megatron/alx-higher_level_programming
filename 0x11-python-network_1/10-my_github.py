ee4a6a391cb7cfc4cfe6145b20a08dac178270c2
7034megatron <emmanuelmabhena0@gmail.com>
2024-04-01 11:53:58 -0700

    added commit

---
+++ 0x11-python-network_1/10-my_github.py
@@ -0,0 +1,17 @@
+import requests
+import sys
+
+def get_github_id(username, password):
+    url = f"https://api.github.com/users/{username}"
+    response = requests.get(url, auth=(username, password))
+    if response.status_code == 200:
+        return response.json()["id"]
+    else:
+        return None
+
+if __name__ == "__main__":
+    username = sys.argv[1]
+    password = sys.argv[2]
+    github_id = get_github_id(username, password)
+    print(github_id)
+
